1e12603eafd16cec492d504808caf9b97a558349
hu1909 <hugn.nq1909@gmail.com>
2021-10-16 00:04:12 +0700

    Midterm project

---
+++ Email/Email/app/src/main/java/vn/edu/usth/email/MainActivity.java
@@ -0,0 +1,32 @@
+package vn.edu.usth.email;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void Login(View view){
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
+    public  void SignUp(View view){
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
+    }
+
+}
---
+++ Email/Email/app/src/main/java/vn/edu/usth/email/Spamcontent.java
@@ -0,0 +1,24 @@
+package vn.edu.usth.email;
+
+public class Spamcontent {
+    private String spammer;
+    private String spamcontent;
+
+    public static final Spamcontent[] spamcontents = {
+            new Spamcontent("Spam1", "something 1"),
+            new Spamcontent("Spam2", "something 2"),
+            new Spamcontent("Spam3", "something 3")
+    };
+
+    private Spamcontent(String spammer, String spamcontent) {
+        this.spammer = spammer;
+        this.spamcontent = spamcontent;
+    }
+    public String getSpammer() {
+        return spammer;
+    }
+    public String getSpamcontent() { return spamcontent; }
+    public String toString() {
+        return this.spammer;
+    }
+}
